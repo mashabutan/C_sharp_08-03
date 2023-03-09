15ae9c769e81d99f74b064df460042f38f32fc63
mashabutan <mashabutan@gmail.com>
2023-03-09 12:30:12 +0300

    1.2

---
+++ Lesson_1/HW 1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход три числа и 
+//выдает максимальное из этих чисел 
+
+Console.Write("Введите первое число: ");
+int a = int.Parse (Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse (Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int c = int.Parse (Console.ReadLine()!);
+
+
+if (a > b)
+{
+    if (a > c)
+    {
+    Console.WriteLine("Максимальное первое число число");
+    }
+    else
+    {
+    Console.WriteLine("Максимальное третье число");
+    
+    }
+
+}
+
+{else if (b > c)
+    Console.WriteLine("Максимальное второе число");
+else 
+    Console.WriteLine("Максимальное третье число");
+}
