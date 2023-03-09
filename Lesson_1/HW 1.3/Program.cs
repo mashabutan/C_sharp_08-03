00f1feb25e514f823c51cefc0657a3d19ea0d789
mashabutan <mashabutan@gmail.com>
2023-03-09 14:08:09 +0300

    task 1.3

---
+++ Lesson_1/HW 1.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдает, 
+//является ли оно четным
+
+Console.Write("Введите число: ");
+int num = int.Parse (Console.ReadLine()!);
+
+if (num %2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+ else
+ {
+    Console.WriteLine("Число нечетное");
+ }
