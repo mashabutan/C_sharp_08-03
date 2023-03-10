98b3844f5216c29bc370e544410a9f7c8e19027f
mashabutan <mashabutan@gmail.com>
2023-03-10 10:29:21 +0300

    task 2.0

--- Lesson_2/2_0/Program.cs
+++ Lesson_2/2_0/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// напишите программу, которая принимает на вход трехзначное число
+//и на выходе показывает последнюю цифру этого числа.
+
+int TakeNum(int num)
+{
+    Console.WriteLine(num);
+    return num %10;
+}
+int th_num = new Random() .Next(100, 1000);
+int result = TakeNum(th_num);
+Console.WriteLine(result);
