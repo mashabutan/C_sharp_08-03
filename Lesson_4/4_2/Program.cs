f851ead2cf3f56bcabb5e939ca6d8344ec643482
Mariia <mariiakasparyan@gmail.com>
2023-03-18 13:25:01 +0300

    4_1

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+int num = GetUserNumber();
+int digits = CountNumbers(num);
+Console.WriteLine(digits);
+
+int GetUserNumber()
+{
+int number = 0;
+while(number == 0)
+{
+Console.Write("Please enter number: ");
+number = int.Parse(Console.ReadLine()!);
+}
+return number;
+}
+
+int CountNumbers(int n)
+{
+int count = 0;
+while (n > 0)
+{
+n = n/10;
+count++;
+}
+return count;
+}
+
