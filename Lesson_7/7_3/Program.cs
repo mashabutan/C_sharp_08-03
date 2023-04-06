d8d9c454847748a3fcfe6247b5a16a65330cb935
Mariia <mariiakasparyan@gmail.com>
2023-04-06 21:52:50 +0300

    task_3

---
+++ Lesson_7/7_3/Program.cs
@@ -0,0 +1,60 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+//    https://4apple.org/summa-diagonalnyh-jelementov-matricy/
+//    https://ru.wikipedia.org/wiki/%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D0%B4%D0%B8%D0%B0%D0%B3%D0%BE%D0%BD%D0%B0%D0%BB%D1%8C
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+int SumDiagonal(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int diag_sum = 0;
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            if (i == j)
+                diag_sum += arr[i, j];
+
+    return diag_sum;
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+int result = SumDiagonal(mass);
+Console.WriteLine(result);
+
