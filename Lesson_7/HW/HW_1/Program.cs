73d84030750514861bcad9c146f63d86330ea8bd
Mariia <mariiakasparyan@gmail.com>
2023-04-06 22:16:00 +0300

    HW_1

---
+++ Lesson_7/HW/HW_1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// 0. Задайте двумерный массив размером m×n, 
+//    заполненный случайными целыми числами. 
+
+
+void Print(double [,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j], 6} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double [,] MassNums(int row, int column, int from, int to)
+{
+    double [,] arr = new double [row, column];
+    Random  n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(n_new.NextDouble() * (to - from)+ from, 2);
+    return arr;
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+
